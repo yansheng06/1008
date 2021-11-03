95cd34c8263a5edea099633f49051911f27f657e
LAPTOP-LTJLA0SD\yansheng <C107152348@nkust.edu.tw>
2021-11-03 23:01:19 +0800

    1020 sql

--- 1008/1008/Program.cs
+++ 1008/1008/Program.cs
@@ -15,10 +15,10 @@ namespace _1008
 
 			Console.WriteLine(string.Format("分析完成，共有{0}筆資料", csvDatas.Count));
 			var groupDatas = csvDatas.GroupBy(x => x.行政區, y => y).ToList();
-			csvDatas.ForEach(x=>
+			csvDatas.ForEach(x =>
 			{
 				Console.WriteLine($"十碼章 : {x.十碼章} 醫院: {x.醫院名稱} 行政區: {x.行政區} 地址:{x.地址} 連絡電話:{x.連絡電話} 座標緯度 : {x.座標緯度} 座標經度 : {x.座標經度}");
-			})
+			});
 	     Console.ReadKey();
 		}
 	}
---
+++ 1008/1008/Services/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1008.Services
+{
+	class DataService
+	{
+		private string _connectionString = @"Server=.;Database=NKUST;Trusted_Connection=True";
+
+
+	public void GetHospitals()
+	{
+			SqlConnection cn = new SqlConnection(_connectionString);
+
+			cn.Open();
+
+			SqlCommand command = new SqlCommand("Select * from hospital1", cn);
+
+			var reader = command.ExecuteReader();
+			while (reader.Read())
+			{
+				var id = reader["id"];
+				var tencode = reader["tencode"];
+				var name = reader["name"];
+				var district = reader["district"];
+				var address = reader["address"];
+				var phone = reader["phone"];
+			}
+			cn.Close();
+	}
+}
+	}
+	
