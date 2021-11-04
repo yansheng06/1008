38b5b5d04f48cdc397d7a38b6c0ee7b3399ae101
LAPTOP-LTJLA0SD\yansheng <C107152348@nkust.edu.tw>
2021-11-04 23:30:51 +0800

    1029 Dataservice insert

--- 1008/1008/Program.cs
+++ 1008/1008/Program.cs
@@ -1,4 +1,5 @@
-﻿using _1008.Utils;
+﻿using _1008.Services;
+using _1008.Utils;
 using System;
 using System.Linq;
 
@@ -11,11 +12,17 @@ namespace _1008
 			//Console.WriteLine("Hello World!");
 			var/*string*/ fullFilename = FilePath.GetFullPath("fluvaccine.csv");
 			var csvService = new ConsoleApp.Services.ImportCsvService();
+			DataService dataService = new DataService();
 			var csvDatas = csvService.LoadFormFile("fluvaccine.csv");
-
-			Console.WriteLine(string.Format("分析完成，共有{0}筆資料", csvDatas.Count));
+			
 			var groupDatas = csvDatas.GroupBy(x => x.行政區, y => y).ToList();
-			csvDatas.ForEach(x =>
+			csvDatas.ForEach(i => 
+			{
+				dataService.Insert(i);
+			});
+			var rows=dataService.GetHospitals();
+			Console.WriteLine(string.Format("分析完成，共有{0}筆資料", csvDatas.Count));
+			rows.ForEach(x =>
 			{
 				Console.WriteLine($"十碼章 : {x.十碼章} 醫院: {x.醫院名稱} 行政區: {x.行政區} 地址:{x.地址} 連絡電話:{x.連絡電話} 座標緯度 : {x.座標緯度} 座標經度 : {x.座標經度}");
 			});
--- 1008/1008/Services/DataService.cs
+++ 1008/1008/Services/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1008.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -12,26 +13,57 @@ namespace _1008.Services
 		private string _connectionString = @"Server=.;Database=NKUST;Trusted_Connection=True";
 
 
-	public void GetHospitals()
+	public List<Hospital> GetHospitals()
 	{
+			var result = new List<Hospital>();
 			SqlConnection cn = new SqlConnection(_connectionString);
 
 			cn.Open();
 
 			SqlCommand command = new SqlCommand("Select * from hospital1", cn);
-
-			var reader = command.ExecuteReader();
-			while (reader.Read())
+			
+			var indexer = command.ExecuteReader();
+			while (indexer.Read())
 			{
-				var id = reader["id"];
-				var tencode = reader["tencode"];
-				var name = reader["name"];
-				var district = reader["district"];
-				var address = reader["address"];
-				var phone = reader["phone"];
+				var id = indexer["id"];
+				var tencode = indexer["tencode"];
+				var name = indexer["name"];
+				var district = indexer["district"];
+				var address = indexer["address"];
+				var phone = indexer["phone"];
+				var item = new Hospital()
+				{ 
+					 十碼章=tencode.ToString(),
+					 醫院名稱=name.ToString(),
+					 行政區=district.ToString(),
+					 地址=address.ToString(),
+					 連絡電話=phone.ToString(),
+					 //Id=long.Parse(id.ToString())
+				};
+				result.Add(item);
 			}
 			cn.Close();
+			return result;
 	}
+	public void Insert(Hospital hospital) 
+		{
+			var commandString = @"
+			INSERT INTO [dbo].[hospital1]([tencode],[name],[district],[address],[phone])
+			VALUES(@十碼章,@醫院名稱,@行政區,@地址,@)";
+
+			SqlConnection cn = new SqlConnection(_connectionString);
+
+			cn.Open();
+			SqlCommand command = new SqlCommand(commandString, cn);
+
+			var count=command.ExecuteNonQuery();
+			command.Parameters.Add(new SqlParameter("十碼章", hospital.十碼章));
+			command.Parameters.Add(new SqlParameter("醫院名稱", hospital.醫院名稱));
+			command.Parameters.Add(new SqlParameter("行政區", hospital.行政區));
+			command.Parameters.Add(new SqlParameter("地址", hospital.地址));
+			command.Parameters.Add(new SqlParameter("連絡電話", hospital.連絡電話));
+			cn.Close();
+		}
 }
 	}
 	
